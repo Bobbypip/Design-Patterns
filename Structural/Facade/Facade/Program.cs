a1e6846d546f16aa493ed158916de865dce7f8d1
Bobby <roroal314@gmail.com>
2023-02-16 21:42:03 -0600

    Patterns done
    Estructural/Decorator  Done
    Estructural/Facade Done
    Estructural/Flyweight  Done
    Estructural/Proxy  Done

---
+++ Structural/Facade/Facade/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public class HomeController 
+        {
+            private WifiController _wifiController = new WifiController();
+            private AirConditionerController _airConditionerController = new AirConditionerController();
+            private LightController _lightController = new LightController();
+
+            public void TurnOn()
+            {
+                _wifiController.TurnOn();
+                _airConditionerController.TurnOn();
+                _lightController.TurnOn();
+            }
+
+            public void TurnOff()
+            {
+                _wifiController?.TurnOff();
+                _airConditionerController?.TurnOff();
+                _lightController?.TurnOff();
+            }
+        }
+        public class WifiController
+        {
+            public void TurnOn()
+            {
+                Console.WriteLine("Wifi is on");
+            }
+
+            public void TurnOff()
+            {
+                Console.WriteLine("Wifi is off");
+            }
+        }
+
+        public class AirConditionerController
+        {
+            public void TurnOn()
+            {
+                Console.WriteLine("Aire acondicionado is on");
+            }
+
+            public void TurnOff()
+            {
+                Console.WriteLine("Aire acondicionado is off");
+            }
+        }
+
+        public class LightController 
+        {
+            public void TurnOn()
+            {
+                Console.WriteLine("Luz is on");
+            }
+
+            public void TurnOff()
+            {
+                Console.WriteLine("Luz is off");
+            }
+        }
+        static void Main(string[] args)
+        {
+            HomeController homeController = new HomeController();
+
+            Console.WriteLine("LLegando a casa... ");
+            homeController.TurnOn();
+            Console.WriteLine("Saliendo de casa... ");
+            homeController.TurnOff();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Structural/Proxy_1/Proxy_1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("El automovil esta siendo conducido");
+        }
+    }
+
+    public class Driver
+    {
+        private int _age;
+        private bool _hasLicense;
+
+        public Driver(int age, bool hasLicense)
+        {
+            _age = age;
+            _hasLicense = hasLicense;
+        }
+
+        internal bool CanDrive() => _age >= 18 && _hasLicense;
+    }
+
+    public class CarProxy : ICar
+    {
+        private Car _car = new Car();
+        private Driver _driver;
+
+        public CarProxy(Driver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (_driver.CanDrive())
+                _car.Drive();
+            else
+                Console.WriteLine("El conductor no puede conducir");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver(17, true));
+            car.Drive();
+            Console.ReadLine();
+        }
+    }
+
+
+}
