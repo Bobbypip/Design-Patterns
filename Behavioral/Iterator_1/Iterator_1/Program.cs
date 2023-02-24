a1aa9c9e3a7a7b55238a023dd3dc4f069c68fbae
Bobby <roroal314@gmail.com>
2023-02-24 12:42:27 -0600

    Behavioral/Iterator Done

---
+++ Behavioral/Iterator_1/Iterator_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+// Concrete collection class
+class MyCollection : IEnumerable
+{
+    private int[] items;
+
+    public MyCollection(int[] arr)
+    {
+        items = arr;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        // Returns a new instance of the custom iterator class
+        return new MyIterator(items);
+    }
+}
+
+// Custom iterator class
+class MyIterator : IEnumerator
+{
+    private int[] items;
+    private int position = -1;
+
+    public MyIterator(int[] arr)
+    {
+        items = arr;
+    }
+
+    public object Current
+    {
+        get
+        {
+            return items[position];
+        }
+    }
+
+    public bool MoveNext()
+    {
+        // Move to the next item in the collection
+        position++;
+
+        // Return true if there are still items to iterate over
+        return position < items.Length;
+    }
+
+    public void Reset()
+    {
+        // Reset the iterator to its initial state
+        position = -1;
+    }
+}
+
+// Client code
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+        // Create a new instance of the collection class
+        MyCollection collection = new MyCollection(arr);
+
+        // Iterate over the collection using foreach
+        foreach (int item in collection)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
